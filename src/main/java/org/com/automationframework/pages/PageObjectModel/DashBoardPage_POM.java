e2c1a2b28759faded33c7adaf429ad3021348a75
Ramya Bharathi M <ramyabharathim22@gmail.com>
2024-07-25 14:53:05 +0530

    Framework

---
+++ src/main/java/org/com/automationframework/pages/PageObjectModel/DashBoardPage_POM.java
@@ -0,0 +1,4 @@
+package org.com.automationframework.pages.PageObjectModel;
+
+public class DashBoardPage_POM {
+}
---
+++ src/main/java/org/com/automationframework/pages/PageObjectModel/LoginPage_POM.java
@@ -0,0 +1,9 @@
+package org.com.automationframework.pages.PageObjectModel;
+
+public class LoginPage_POM {
+
+    //PAGE LOCATORS
+
+
+    //PAGE ACTIONS
+}
